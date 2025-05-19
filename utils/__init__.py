65a83c95f176cdbbf92b25b9f117b8c27df0db03
hidarfaqeeh <hidarfaqeeh@gmail.com>
2025-05-19 06:27:10 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,13 @@
+def get_badge(rank):
+    if rank == 1:
+        return "🥇"
+    elif rank == 2:
+        return "🥈"
+    elif rank == 3:
+        return "🥉"
+    return ""
 
+def format_username(user_id, username):
+    if username:
+        return f"@{username}"
+    return f"<a href='tg://user?id={user_id}'>{user_id}</a>"
